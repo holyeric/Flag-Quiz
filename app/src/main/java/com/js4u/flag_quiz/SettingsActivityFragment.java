e7c09149e5987d3c10bfccb3ff2c000c3f17b454
eric <holyeric@yahoo.com>
2016-04-24 12:02:32 +0800

    Finish UI.

--- app/src/main/java/com/js4u/flag_quiz/SettingsActivityFragment.java
+++ app/src/main/java/com/js4u/flag_quiz/SettingsActivityFragment.java
@@ -16,7 +16,7 @@ public class SettingsActivityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
